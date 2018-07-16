54d55a855fcb7a5573bccb6a42ef73e538804f63
USER <USER@com201-05>
2018-07-16 18:22:11 +0900

    업데이트11

--- javabean/src/main/java/com/jb/jbean/controller/ProductController.java
+++ javabean/src/main/java/com/jb/jbean/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.jb.jbean.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.jb.jbean.domain.ProductVo;
 import com.jb.jbean.service.ProductService;
 
 
@@ -20,7 +23,11 @@ public class ProductController {
 	@RequestMapping(value="/")
 	protected String main(Model model) {
 		
-		System.out.println("-------------sllls-----------------------------------");
+		System.out.println("----main---------------------------------------");
+		ArrayList<ProductVo> alist = pros.category();
+		ArrayList<ProductVo> alist2 = pros.productAll();
+		model.addAttribute("alist", alist);
+		model.addAttribute("alist2", alist2);
 		
 		return "main";
 	}
