7f6fcd8e7e701b9f4ec7bc710c8a01c836095677
test0 <hll5239@test.com>
2018-07-20 15:31:12 +0900

    커밋

--- javabean/src/main/java/com/jb/jbean/service/ProductService.java
+++ javabean/src/main/java/com/jb/jbean/service/ProductService.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import com.jb.jbean.domain.BuyVo;
 import com.jb.jbean.domain.ProQnaVo;
 import com.jb.jbean.domain.ProductVo;
+import com.jb.jbean.domain.QnaVo;
 
 public interface ProductService {
 	
@@ -20,4 +21,7 @@ public interface ProductService {
 	public ArrayList<ProQnaVo> qnaList(int pronum);
 	public int qnaWrite(ProQnaVo proqvo);
 	public int qnaMemberDelete(int qidx);
+	public int qnaAdminDelete(int qidx);
+	
+	public int qnaReplyWrite(int qidx, String qreply);
 }
